4bc4e18e14b41631f57133907d1502aa8c35f3e4
ime-m <116743275+ime-m@users.noreply.github.com>
2024-03-19 10:14:22 -0400

    box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,9 +1,17 @@
+import './Main.css'
 function Main(){
     return(
-        <> 
-        <main>Main</main>
-        <h1>Meu H1</h1>
-        </>
+        <main>
+           <div className="estudante-box">
+            <img className="estudante_foto" src="https://github.com/ime-m.png" alt=""/>
+            <div className="informações">
+            <h3 className="estudante_nome">Emily Marques da Cruz</h3>
+            <p className="estudante_descricao">Estudante do IFMS em técnico em informática para internet </p>
+            <a className='link-git' href="https://github.com">GitHub</a>
+           </div>
+           </div>
+        </main>
+        
     )
 }
 
